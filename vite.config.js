5db2f2a0518aa919fc4f0a847d4d34a9b9addcf2
Weston Muniak <53122246+swmuniak@users.noreply.github.com>
2025-12-01 22:59:25 -0500

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/weston-muniak-landing/',
+  base: '/westonmuniak-landing/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
